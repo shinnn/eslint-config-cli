9be6321232b51710323deccf9f40a3957543cb52
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-09-14 17:44:22 +0900

    remove Promise polyfill
    [BREAKING] drop support for Node.js < 0.12.0

--- index.js
+++ index.js
@@ -3,14 +3,13 @@
 var unlink = require('fs').unlink;
 
 var pify = require('pify');
-var PinkiePromise = require('pinkie-promise');
 
 var ERROR = 'Expected a path of a file or symbolic link (<string>)';
-var promisifiedUnlink = pify(unlink, PinkiePromise);
+var promisifiedUnlink = pify(unlink);
 
 module.exports = function rmf(path) {
 	if (path === '') {
-		return PinkiePromise.reject(new Error(ERROR + ', but got \'\' (empty string).'));
+		return Promise.reject(new Error(ERROR + ', but got \'\' (empty string).'));
 	}
 
 	return promisifiedUnlink(path).then(function cb() {
