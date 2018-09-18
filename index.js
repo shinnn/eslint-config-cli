33efd0577d0489cfa455e6823d1d107af602f8af
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2018-09-18 09:39:24 +0900

    export async function
    [BREAKING] drop support for Node.js < 7

--- index.js
+++ index.js
@@ -8,34 +8,38 @@ const ARGLEN_ERROR = 'Expected 1 argument (<string>)';
 const ERROR = 'Expected a path of a file or symbolic link (<string>)';
 const promisifiedUnlink = pify(unlink);
 
-module.exports = function rmf(...args) {
+module.exports = async function rmf(...args) {
 	const argLen = args.length;
 
 	if (argLen === 0) {
 		const error = new RangeError(`${ARGLEN_ERROR}, but got no arguments.`);
 		error.code = 'ERR_MISSING_ARGS';
 
-		return Promise.reject(error);
+		throw error;
 	}
 
 	if (argLen !== 1) {
 		const error = new RangeError(`${ARGLEN_ERROR}, but got ${argLen} arguments.`);
 		error.code = 'ERR_TOO_MANY_ARGS';
 
-		return Promise.reject(error);
+		throw error;
 	}
 
 	const [path] = args;
 
 	if (path === '') {
-		return Promise.reject(new Error(`${ERROR}, but got '' (empty string).`));
+		throw new Error(`${ERROR}, but got '' (empty string).`);
 	}
 
-	return promisifiedUnlink(path).then(() => true, err => {
+	try {
+		await promisifiedUnlink(path);
+	} catch (err) {
 		if (err.code === 'ENOENT') {
 			return false;
 		}
 
 		throw err;
-	});
+	}
+
+	return true;
 };
